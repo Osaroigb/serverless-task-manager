547e97d9d6608c9ff3c712bac8e5ddd4690b1fb0
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-10-17 09:59:03 +0100

    test: add integration test for project

--- test/serverless-task-manager.test.ts
+++ test/serverless-task-manager.test.ts
@@ -1,17 +1,114 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as ServerlessTaskManager from '../lib/serverless-task-manager-stack';
+import '@aws-cdk/assert/jest';
+import * as cdk from '@aws-cdk/core';
+import { ServerlessTaskManagerStack } from '../lib/serverless-task-manager-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/serverless-task-manager-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new ServerlessTaskManager.ServerlessTaskManagerStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('ServerlessTaskManagerStack', () => {
+  let app: cdk.App;
+  let stack: ServerlessTaskManagerStack;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new ServerlessTaskManagerStack(app, 'TestStack');
+  });
+
+  test('should create a DynamoDB table with correct properties', () => {
+    expect(stack).toHaveResource('AWS::DynamoDB::Table', {
+      BillingMode: 'PAY_PER_REQUEST',
+      AttributeDefinitions: [
+        {
+          AttributeName: 'taskId',
+          AttributeType: 'S',
+        },
+      ],
+      KeySchema: [
+        {
+          AttributeName: 'taskId',
+          KeyType: 'HASH',
+        },
+      ],
+    });
+  });
+
+  test('should create the create-task Lambda function with correct properties', () => {
+    expect(stack).toHaveResource('AWS::Lambda::Function', {
+      Handler: 'lambdas/create-task.handler',
+      Runtime: 'nodejs14.x',
+      MemorySize: 128,
+    });
+  });
+
+  test('should create the get-task Lambda function with correct properties', () => {
+    expect(stack).toHaveResource('AWS::Lambda::Function', {
+      Handler: 'lambdas/get-task.handler',
+      Runtime: 'nodejs14.x',
+      MemorySize: 128,
+    });
+  });
+
+  // Test Update Task Lambda function creation
+  test('should create the update-task Lambda function with correct properties', () => {
+    expect(stack).toHaveResource('AWS::Lambda::Function', {
+      Handler: 'lambdas/update-task.handler',
+      Runtime: 'nodejs14.x',
+      MemorySize: 128,
+    });
+  });
+
+  // Test Delete Task Lambda function creation
+  test('should create the delete-task Lambda function with correct properties', () => {
+    expect(stack).toHaveResource('AWS::Lambda::Function', {
+      Handler: 'lambdas/delete-task.handler',
+      Runtime: 'nodejs14.x',
+      MemorySize: 128,
+    });
+  });
+
+  // Test API Gateway creation with correct properties
+  test('should create an API Gateway with correct properties', () => {
+    expect(stack).toHaveResource('AWS::ApiGateway::RestApi', {
+      Name: 'Tasks Service',
+    });
+  });
+
+  // Test IAM Role creation for Lambda functions
+  test('should create IAM roles with correct policies for Lambda functions', () => {
+    expect(stack).toHaveResource('AWS::IAM::Role', {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Effect: 'Allow',
+            Principal: {
+              Service: 'lambda.amazonaws.com',
+            },
+            Action: 'sts:AssumeRole',
+          },
+        ],
+      },
+    });
+
+    expect(stack).toHaveResource('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: expect.arrayContaining([
+          expect.objectContaining({
+            Action: expect.arrayContaining([
+              'dynamodb:GetItem',
+              'dynamodb:PutItem',
+              'dynamodb:UpdateItem',
+              'dynamodb:DeleteItem',
+            ]),
+            Effect: 'Allow',
+          }),
+        ]),
+      },
+    });
+  });
+
+  test('should export the DynamoDB table name', () => {
+    expect(stack).toHaveOutput({
+      OutputName: 'TaskTableName',
+      Value: {
+        'Fn::GetAtt': ['TestStackDynamoDBTable', 'Arn'],
+      },
+    });
+  });
 });
