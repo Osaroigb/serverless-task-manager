267916f8a74af7c74a93861306c877d4f6c48f4e
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-10-17 07:23:30 +0100

    feat: create DynamoDB table for storing tasks

--- lib/serverless-task-manager-stack.ts
+++ lib/serverless-task-manager-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export class ServerlessTaskManagerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessTaskManagerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create DynamoDB Table
+    const tasksTable = new dynamodb.Table(this, 'TasksTable', {
+      partitionKey: { name: 'taskId', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      tableName: 'TasksTable',
+    });
   }
 }
