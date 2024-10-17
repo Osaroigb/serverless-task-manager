a9a5b5108e26a4818a63edbad17ae2d8a65d2dd7
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-10-17 07:29:10 +0100

    feat: Create Lambda function for 'Create Task' operation

--- lib/serverless-task-manager-stack.ts
+++ lib/serverless-task-manager-stack.ts
@@ -1,6 +1,9 @@
+import { join } from 'path';
 import * as cdk from '@aws-cdk/core';
 import { Construct } from 'constructs';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 
 export class ServerlessTaskManagerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,6 +15,15 @@ export class ServerlessTaskManagerStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       tableName: 'TasksTable',
+    });
+
+    const createTaskLambda = new NodejsFunction(this, 'CreateTaskLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handler',
+      entry: join(__dirname, '../lambdas/create-task.ts'),
+      environment: {
+        TABLE_NAME: tasksTable.tableName,
+      },
     });
   }
 }
